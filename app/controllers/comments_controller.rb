e7f23dc0d8a728879205ae2516956fe41368bb8b
Lindsey Ullman <lindsey.ullman@gmail.com>
2016-02-28 18:08:04 -0800

    Update controllers with http authentication parameters

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,6 +1,6 @@
 class CommentsController < ApplicationController
 
-  http_basic_authenticate_with name: "dhh", password: "secret", only: :destroy
+  http_basic_authenticate_with name: "lindsey", password: "Test!ng13", only: :destroy
   
   def create
     @article = Article.find(params[:article_id])
